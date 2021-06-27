e204935acb7d1e6860ae49cbb41bca8ab0967980
Gerald Versluis <gerald@verslu.is>
2021-06-27 15:01:13 +0200

    Update TodoItem.cs

--- XFRefitSample/TodoItem.cs
+++ XFRefitSample/TodoItem.cs
@@ -3,8 +3,11 @@
     public class TodoItem
     {
         public int UserId { get; set; }
+
         public int Id { get; set; }
+
         public string Title { get; set; }
-        public string Completed { get; set; }
+
+        public bool Completed { get; set; }
     }
 }
